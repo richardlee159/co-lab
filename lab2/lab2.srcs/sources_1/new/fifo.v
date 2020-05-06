cc60915fef440e7cafa3844fe5a532bce50cd1c0
Richard <richardlee@qq.com>
2020-05-06 19:39:16 +0800

    FIFO: change ram type & output pattern

--- lab2/lab2.srcs/sources_1/new/fifo.v
+++ lab2/lab2.srcs/sources_1/new/fifo.v
@@ -31,35 +31,42 @@ module fifo(
     wire en_in_edge, en_out_edge;
     wire [3:0] addr, addr_inc, head_q, tail_q;
     wire [4:0] ctr_q, ctr_d, addend;
-    reg ram_en, ram_we;
-    reg head_en, tail_en, ctr_en;
+    wire [7:0] memout;
+    reg ram_we;
+    reg head_en, tail_en, ctr_en, outr_en;
     reg addr_src, inc_dec;
     
     /* ---------- DATA PATH ---------- */
+    
+    // 取信号上升沿
     signal_edge IN_EDGE(clk, en_in, en_in_edge);
     signal_edge OUT_EDGE(clk, en_out, en_out_edge);
     
-    blk_mem_16x8 MEM(
-        .clka(clk),
-        .addra(addr),
-        .dina(din),
-        .douta(dout),
-        .ena(ram_en),
-        .wea(ram_we)
+    // 分布式ram
+    dist_mem_16x8 MEM(
+        .clk(clk),
+        .a(addr),
+        .d(din),
+        .spo(memout),
+        .we(ram_we)
     );
+    // OUTR只在出队时更新值，因此dout永远是上次出队的数据
+    register #(8) OUTR(dout, memout, clk, rst, outr_en);
     
+    // 队头队尾指针部分
     assign addr_inc = addr + 1;
     register #(4) HEAD(head_q, addr_inc, clk, rst, head_en);
     register #(4) TAIL(tail_q, addr_inc, clk, rst, tail_en);
     mux2 #(4) ADDRMUX(addr, head_q, tail_q, addr_src);
     
+    // 队列数据计数
     assign ctr_d = ctr_q + addend;
     register #(5) COUNTER(ctr_q, ctr_d, clk, rst, ctr_en);
     mux2 #(5) CTRMUX(addend, 5'd1, -5'd1, inc_dec);
     assign count = ctr_q;
     
     /* ---------- CONTROL UNIT ---------- */
-    localparam WAIT = 2'b00, ENQ = 2'b01, DEQ = 2'b10, HOLD = 2'b11;
+    localparam WAIT = 2'b00, ENQ = 2'b01, DEQ = 2'b10;
     // state register
     reg [1:0] current_state, next_state;
     always @(posedge clk, posedge rst) begin
@@ -77,18 +84,17 @@ module fifo(
                 else if (en_out_edge && (ctr_q != 5'd0)) next_state = DEQ;
                 else next_state = WAIT;
             end
-            ENQ, DEQ : next_state = HOLD;
+            ENQ, DEQ : next_state = WAIT;
             default : next_state = WAIT;
         endcase
     end
     
     // output logic
     always @(*) begin
-        {ram_en, ram_we, head_en, tail_en, ctr_en, addr_src, inc_dec} = 7'b0000000;
+        {ram_we, head_en, tail_en, ctr_en, outr_en, addr_src, inc_dec} = 7'b0;
         case (current_state)
-            ENQ : {ram_en, ram_we, tail_en, ctr_en, addr_src} = 5'b11111;
-            DEQ : {ram_en, head_en, ctr_en, inc_dec} = 4'b1111;
-            HOLD : ram_en = 1;
+            ENQ : {ram_we, tail_en, ctr_en, addr_src} = 4'b1111;
+            DEQ : {head_en, ctr_en, outr_en, inc_dec} = 4'b1111;
         endcase
     end
     
